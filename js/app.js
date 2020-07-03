8fa23e30d7d7352fe657b06baaf0eab257b45a1e
wileyJak3 <wileyJak3@gmail.com>
2020-07-03 18:13:43 -0400

    set canvas witdth/height to variables and created context

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 //* going to search entire html document and look for and html element called canvas
 let canvas = document.querySelector('canvas')
+
+
+//* sets the length and width of the entire canvas to the window length and width
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let context = canvas.getContext('2d');
